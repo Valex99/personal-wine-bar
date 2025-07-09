c41c4b18bccee6ac1e7b62364bbca474c7547672
valex <mval.fx@gmail.com>
2025-07-09 10:31:09 +0200

    Set nav layout

--- src/app/[locale]/(front)/header.tsx
+++ src/app/[locale]/(front)/header.tsx
@@ -3,16 +3,23 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center">
-      <Link href="/">
-        <Image src="/logo1.png" alt="Logo" width={100} height={100} />
+    <div className="absolute bottom-10 left-1/2 -translate-x-1/2 z-10 flex justify-between items-center bg-wine-bar-white rounded-full p-2 px-5 min-w-[500px] h-[50px]">
+      <Link href="/" className="flex h-full">
+        <Image src="/logo2.png" alt="Logo" width={30} height={30} />
       </Link>
-      <div className="flex gap-4">
-        <Link href="/tastings">Tastings</Link>
-        <Link href="/events">Events</Link>
-        <Link href="/gallery">Gallery</Link>
-        <Link href="/reservations">Reservations</Link>
-      </div>
+
+      <Link href="/tastings" className="text-wine-bar-red">
+        Tastings
+      </Link>
+      <Link href="/events" className="text-wine-bar-red">
+        Events
+      </Link>
+      <Link href="/gallery" className="text-wine-bar-red">
+        Gallery
+      </Link>
+      <Link href="/reservations" className="text-wine-bar-red">
+        Reservations
+      </Link>
     </div>
   );
 }
--- src/app/[locale]/(front)/layout.tsx
+++ src/app/[locale]/(front)/layout.tsx
@@ -1,7 +1,14 @@
+import Header from "./header";
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 }
